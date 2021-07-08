e4352c7355931d994577855f2f166c403b630444
nathan <romeronathan@hotmail.com>
2021-07-08 01:04:08 -0400

    Change picture home page

--- src/app/components/navbar/nav-popup-dialog/nav-popup-dialog.component.ts
+++ src/app/components/navbar/nav-popup-dialog/nav-popup-dialog.component.ts
@@ -10,20 +10,12 @@ import { Location } from '@angular/common'
 export class NavPopupDialogComponent implements OnInit {
   constructor(private dialogRef: MatDialogRef<NavPopupDialogComponent>, private location: Location) {}
   links: Link[];
-  socialLinks: Link[];
-  ngOnInit(): void {
+   ngOnInit(): void {
     this.links = [
       { name: 'Home', href: '#about-page' },
       { name: 'Technologies', href: '#tech-page' },
       { name: 'Team', href: '#team-page-mobile' },
       { name: 'Contact', href: '#contact-page-mobile'},
-    ];
-    this.socialLinks = [
-      { name: 'facebook', href: '/' },
-      { name: 'discord', href: '/' },
-      { name: 'instagram', href: '#' },
-      { name: 'linkedin', href: '#' },
-
     ];
   }
   closeDialog() {
