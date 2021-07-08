47fc0ea8c43efe6f2f46b3e521133c13c37e5e54
nathan <romeronathan@hotmail.com>
2021-07-07 22:39:44 -0400

    Fixed mobile navbar routing

--- src/app/components/navbar/nav-popup-dialog/nav-popup-dialog.component.ts
+++ src/app/components/navbar/nav-popup-dialog/nav-popup-dialog.component.ts
@@ -1,22 +1,22 @@
 import { Component, ElementRef, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Link } from 'src/app/types';
-
+import { Location } from '@angular/common'
 @Component({
   selector: 'app-nav-popup-dialog',
   templateUrl: './nav-popup-dialog.component.html',
   styleUrls: ['./nav-popup-dialog.component.scss'],
 })
 export class NavPopupDialogComponent implements OnInit {
-  constructor(private dialogRef: MatDialogRef<NavPopupDialogComponent>) {}
+  constructor(private dialogRef: MatDialogRef<NavPopupDialogComponent>, private location: Location) {}
   links: Link[];
   socialLinks: Link[];
   ngOnInit(): void {
     this.links = [
-      { name: 'Home', href: 'about-page' },
-      { name: 'Technologies', href: 'tech-page' },
-      { name: 'Team', href: 'team-page' },
-      { name: 'Contact', href: 'contact-page'},
+      { name: 'Home', href: '#about-page' },
+      { name: 'Technologies', href: '#tech-page' },
+      { name: 'Team', href: '#team-page-mobile' },
+      { name: 'Contact', href: '#contact-page'},
     ];
     this.socialLinks = [
       { name: 'facebook', href: '/' },
@@ -29,12 +29,13 @@ export class NavPopupDialogComponent implements OnInit {
   closeDialog() {
     this.dialogRef.close();
   }
-  goToPage(name: any) {
-    this.dialogRef.close();
-    var doc = document.getElementById(name);
-    if (doc) {
-      doc.scrollIntoView({ behavior: 'smooth' });
-    }
+  goToPage() {
+    setTimeout(()=>{
+      this.dialogRef.close();
+      this.location.replaceState("")
+    }, 5); 
+
+   
   }
 
 }
