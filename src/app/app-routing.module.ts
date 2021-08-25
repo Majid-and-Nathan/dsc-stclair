df5db20e3a9459ddc2beb03a34ed5d5e08e1999e
nathan <romeronathan@hotmail.com>
2021-08-25 12:23:54 -0400

    Another fix for live

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { ContactUsComponent } from './components/pages/contact-us/contact-us.com
 import { HomeComponent } from './components/pages/home/home.component';
 
 const routes: Routes = [
+  { path: "", component: HomeComponent },
   { path: "contact-us", component: ContactUsComponent }
 ];
 
