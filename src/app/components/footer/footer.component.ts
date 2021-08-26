e3ecd29d0c7303afe7baf96d4e3a9bec6e4da4b9
nathan <romeronathan@hotmail.com>
2021-08-25 22:17:16 -0400

    Title fixes

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -13,6 +13,18 @@ export class FooterComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.icons.push({
+      name: "fab fa-discord",
+      href: "https://discord.gg/fTBhdtmY"
+    });
+    this.icons.push({
+      name: "fab fa-github",
+      href: "https://github.com/dsc-stclair"
+    });
+    this.icons.push({
+      name: "fab fa-linkedin",
+      href: "https://www.linkedin.com/company/developer-student-club-st-clair-college/"
+    });
 
 
     this.credit = `${(new Date()).getFullYear().toString()} | All rights reserved.`;
